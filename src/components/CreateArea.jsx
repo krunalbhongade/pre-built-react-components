04914a7a5c3aad5eb3ed31a622a5373ec48db699
Krunal <126875304+krunalbhongade@users.noreply.github.com>
2023-09-25 18:27:39 +0530

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import Fab from "@mui/material/Fab";
+
+function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  function expand() {
+    setExpanded(true);
+  }
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+
+        <Fab>
+          <AddCircleOutlineIcon onClick={submitNote} />
+        </Fab>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
